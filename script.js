68eecca55c5c44829fce37ca5daf3a4117f63f1c
Soumya Mandal <73708214+Soumyalpha@users.noreply.github.com>
2023-11-04 18:41:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
+const monthsEl = document.getElementById('months');
 const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
 
+
 const newYear = '1 Jan 2024';
 
 function countdown() {
-    const newYearDate = new Date(newYear);
-    const currentDate = new Date();
+    const targetdate = new Date(newYear);
+    const currentDate = new Date().getTime();
+    const timeremaining = targetdate - currentDate;
 
-    const totalseconds = (newYearDate - currentDate) / 1000;
-
-    const days = Math.floor(totalseconds / 3600 / 24);
+    const totalseconds = (targetdate - currentDate) / 1000;
+    
+    const months = Math.floor(timeremaining/(1000 * 60 * 60 * 24 * 30.44)) ;
+    const days1 = Math.floor(totalseconds / 3600 / 24);
+    const days = days1 - 30 ;
     const hours = Math.floor(totalseconds / 3600) % 24;
     const minutes = Math.floor(totalseconds / 60) % 60;
     const seconds = Math.floor(totalseconds) % 60;
 
+    monthsEl.innerHTML = formatTime(months);
     daysEl.innerHTML = formatTime(days);
     hoursEl.innerHTML = formatTime(hours);
     minutesEl.innerHTML = formatTime(minutes);
