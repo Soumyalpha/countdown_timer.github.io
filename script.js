1d28bbf04f8102df4ac1950b8e82d201763035e1
Soumya Mandal <73708214+Soumyalpha@users.noreply.github.com>
2023-11-05 08:00:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,12 +11,19 @@ function countdown() {
     const targetdate = new Date(newYear);
     const currentDate = new Date().getTime();
     const timeremaining = targetdate - currentDate;
+    const currentmonth = new Date().getMonth() + 1;
 
     const totalseconds = (targetdate - currentDate) / 1000;
     
     const months = Math.floor(timeremaining/(1000 * 60 * 60 * 24 * 30.44)) ;
     const days1 = Math.floor(totalseconds / 3600 / 24);
-    const days = days1 - 30 ;
+    if (currentmonth % 2 == 0) {
+        var days = (days1 - 31) ;
+    } else if (currentmonth % 2 != 0 ){
+        var days = days1 - 30; 
+    } else if (currentmonth == 2) {
+        var days = days1 - 28;
+    }
     const hours = Math.floor(totalseconds / 3600) % 24;
     const minutes = Math.floor(totalseconds / 60) % 60;
     const seconds = Math.floor(totalseconds) % 60;
